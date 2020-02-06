7da5ed10f974eb99f307ae1f05e7a445acbaeb81
18150028757 <18150028757@163.com>
2020-02-06 20:34:12 +0800

    commitTest

---
+++ src/main/java/com/cwd/WxProApplication.java
@@ -0,0 +1,13 @@
+package com.cwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxProApplication.class, args);
+    }
+
+}
