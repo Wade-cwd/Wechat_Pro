37f4230d07f528feb62d9a39d4c4760e5fe81335
18150028757 <18150028757@163.com>
2020-02-10 17:18:16 +0800

    commitTest

--- src/main/java/com/cwd/WxProApplication.java
+++ src/main/java/com/cwd/WxProApplication.java
@@ -3,8 +3,9 @@ package com.cwd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class WxProApplication {
 
     public static void main(String[] args) {
